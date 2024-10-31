04460205701cdd6fc54bd169b4547408c980c781
Coded-Adil <itsadil2022@gmail.com>
2024-10-31 17:26:27 +0600

    tried some reausabnle and custom handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import './App.css'
-import HookForm from './components/HookForm/HookForm'
+import ReusableForm from './components/ReusableForm/ReusableForm'
+// import HookForm from './components/HookForm/HookForm'
 // import RefForm from './components/RefForm/RefForm'
 // import StatefulForm from './components/StatefulForm/StatefulForm'
 // import SimpleForm from './components/SimpleForm/SimpleForm'
 
 function App() {
+
+  const handleSignUpSubmit = data => {
+    console.log(data);
+  }
+  const handleProfileUpdate = data => {
+    console.log(data);
+  }
 
   return (
     <>
@@ -12,7 +20,19 @@ function App() {
       {/* <SimpleForm></SimpleForm> */}
       {/* <StatefulForm></StatefulForm> */}
       {/* <RefForm></RefForm> */}
-      <HookForm></HookForm>
+      {/* <HookForm></HookForm> */}
+      <ReusableForm formTitle={'Sign Up'} handleSubmit={handleSignUpSubmit}>
+        <div>
+          <h2>SignUp</h2>
+          <p>Sign Up this form to get forward</p>
+        </div>
+      </ReusableForm>
+      <ReusableForm formTitle={'Profile Update'} submitBtnText='Update' handleSubmit={handleProfileUpdate}>
+        <div>
+          <h2>Profile Update</h2>
+          <p>Always keep your Profile Updated</p>
+        </div>
+      </ReusableForm>
     </>
   )
 }
